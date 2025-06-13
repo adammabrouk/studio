c27df08d4be90175d2884b95b624e1a1ce602144
Adam Mabrouk <adam@aigents.services>
2025-06-13 17:31:13 +0000

    changing menu display

--- src/components/menu-display.tsx
+++ src/components/menu-display.tsx
@@ -3,15 +3,18 @@
 
 const MenuDisplay = () => {
   return (
-    <div className="fixed inset-0 w-screen h-screen -z-10">
-      <iframe
-        src="/menu.pdf"
-        className="w-full h-full opacity-90"
-        title="Amano restaurant Menu PDF"
-      ></iframe>
+    <div className="fixed inset-0 -z-10 overflow-auto">
+      <div className="w-screen h-screen">
+        <iframe
+          src="/menu.pdf"
+          className="w-full h-full opacity-90"
+          title="Amano restaurant Menu PDF"
+        ></iframe>
+      </div>
     </div>
   );
 };
 
 export default MenuDisplay;
+
 
